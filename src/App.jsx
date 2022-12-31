2797db9406e7b5166703b758e64231c3c9fa9ed4
ita5478 <ittai.paz@gmail.com>
2022-12-31 18:16:33 +0200

    Add game config to app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
 import './App.css'
+import { GameConfig } from './models/GameConfig'
+
+const GAME_CONFIG = new GameConfig({
+  jumpSpeed: 10,
+  gravity: 10,
+  basePosition: {
+    x: 40,
+    y: 100
+  },
+  horizontalSpeed: 10,
+  updateMs: 100
+});
 
 function App() {
 
